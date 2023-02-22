ad8ce3efd3c7c95e8ae5b0b3f63793542389e3d0
Musab Hassan <musabhassan1999@gmail.com>
2023-02-22 22:40:22 +0530

    update

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const app = express();
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended : true}));
 app.use(express.static("public"));
+mongoose.set('strictQuery', true);
 
 main().catch(err => console.log(err));
 async function main() {
